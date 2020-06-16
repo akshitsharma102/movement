bc3cc761842aa84400eb49106c2889899c89a82f
Akshit Sharma <waprajbhasha9@gmail.com>
2020-06-16 14:26:38 +0530

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float runSpeed;
+    private float MoveInput;
+
+    private Rigidbody2D rb2d;
+    private bool facingRight;
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        MoveInput = Input.GetAxisRaw("Horizontal") * runSpeed;
+        rb2d.velocity = new Vector2(MoveInput, rb2d.velocity.y);
+
+        if(MoveInput > 0 && facingRight || MoveInput < 0 && !facingRight)
+        {
+            flip();
+        }
+    }
+
+    void flip ()
+    {
+        facingRight = !facingRight;
+        Vector2 transformScale = transform.localScale;
+        transformScale.x *= -1;
+        transform.localScale = transformScale;
+
+    }
+}
