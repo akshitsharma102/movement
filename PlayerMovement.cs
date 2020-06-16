bea8296318f8da2d16333a4b388f12895159abae
Akshit Sharma <waprajbhasha9@gmail.com>
2020-06-16 19:22:34 +0530

    Add files via upload

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
 
 
     private Rigidbody2D rb2d;
+    private Animator anim;
 
 
     public Transform groundCheck;
@@ -22,6 +23,7 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -33,11 +35,22 @@ public class PlayerMovement : MonoBehaviour
     void Movement()
     {
         MoveInput = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        anim.SetFloat("Speed", Mathf.Abs(MoveInput));
+
         rb2d.velocity = new Vector2(MoveInput, rb2d.velocity.y);
 
         if(MoveInput > 0 && facingRight || MoveInput < 0 && !facingRight)
         {
             flip();
+        }
+        if(rb2d.velocity.y < 0)
+        {
+            anim.SetBool("Fall", true);
+        }
+        else
+        {
+            anim.SetBool("Fall", false);
         }
     }
 
@@ -50,6 +63,11 @@ public class PlayerMovement : MonoBehaviour
             if(bottomHit.gameObject.tag == "ground" && Input.GetKeyDown(KeyCode.Space))
             {
                 rb2d.velocity = new Vector2(rb2d.velocity.x, jumpforce);
+                anim.SetBool("Jump", true);
+            }
+            else
+            {
+                anim.SetBool("Jump", false);
             }
         }
     }
