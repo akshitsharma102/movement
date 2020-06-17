039922457c3b5dd508145bfdb31722175420943c
Akshit Sharma <waprajbhasha9@gmail.com>
2020-06-17 14:20:49 +0530

    Add files via upload

---
+++ playerAttack.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class playerAttack : MonoBehaviour
+{
+    private Animator anim;
+    private bool activeTimeToReset;
+
+    private float defaultComboTimer = .2f, currentComboTimer;
+
+    private int combo;
+
+    public Transform attackPos;
+    public LayerMask enemyLayer;
+
+    public float attackRange;
+    public int daimage;
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Punch();
+        ResetComboState();
+    }
+
+
+
+    void Punch()
+    {
+        if(Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if(combo < 2)
+            {
+                anim.SetBool("Punch", true);
+                activeTimeToReset = true;
+                currentComboTimer = defaultComboTimer;
+
+                //Punch ke lia h
+            }
+            else
+            {
+                anim.SetBool("Punch", false);
+            }
+        }
+    }
+
+    //Ye function combo ++
+
+    public void IncreasComboNumber()
+    {
+        combo++;
+    }
+    //ye FuncN combo ko 0 IF no action\combo++
+    public void ResetCombo()
+    {
+        combo = 0;
+    }
+    // ye function is for reset combo if no markatt
+    void ResetComboState()
+    {
+        if(activeTimeToReset)
+        {
+            currentComboTimer -= Time.deltaTime;
+            if(currentComboTimer <= 0)
+            {
+                anim.SetBool("Punch", false);
+                activeTimeToReset = false;
+                currentComboTimer = defaultComboTimer;
+            }
+        }
+    }
+}
